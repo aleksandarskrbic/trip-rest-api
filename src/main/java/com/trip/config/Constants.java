8f09c9c78815be915c638efbb4d8c3c441a93eae
Aleksandar Skrbic <skrbic.alexa@gmail.com>
2020-07-06 17:16:05 +0200

    Update Constants.java

--- src/main/java/com/trip/config/Constants.java
+++ src/main/java/com/trip/config/Constants.java
@@ -2,6 +2,6 @@ package com.trip.config;
 
 public class Constants {
 
-    public static final String API_KEY = "AIzaSyCDrL-IfnrSlXD2NFI94wrHv4EsvjgPlWg";
+    public static final String API_KEY = "???";
     public static final String BASE_URL = "/api/trip";
 }
